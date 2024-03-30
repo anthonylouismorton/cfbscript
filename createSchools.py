ce0d4b18ce323d54fdb299aec387c8c421f901a5
anthonymorton <anthonymorton760@gmail.com>
2024-03-29 19:22:45 -0700

    added school information and the names of the conferences to the schoolData json

--- createSchools.py
+++ createSchools.py
@@ -9,7 +9,7 @@ schoolArray = []
 
 for conference in data:
     for school in conference["schools"]:
-        obj = {"school": school["school"], "conferences": [{"conference": conference["conference"], "years": school["years"]}]}
+        obj = {"school": school["school"], "logo": school["logo"], "abbreviation": school["abbreviation"], "nickName": school["nickName"], "lon": school["lon"], "lat": school["lat"], "conferences": [{"conference": conference["conference"], "years": school["years"], "names":conference["names"]}]}
         if obj["school"] not in [entry["school"] for entry in schoolArray]:
             schoolArray.append(obj)
         else:
@@ -18,4 +18,4 @@ for conference in data:
                     entry["conferences"].append({"conference": conference["conference"], "years": school["years"]})
 
 with open(school_json, 'w') as handle:
-  json.dump(schoolArray, handle, separators=(',', ':'), indent=None)
+  json.dump(schoolArray, handle, indent=2)
