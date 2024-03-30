ae2e0fc78e5759ac00f3b2499cd2e1df5a3e77db
anthonymorton <anthonymorton760@gmail.com>
2024-03-29 19:06:16 -0700

    successfully extracted the schols from the conference data json and created a new json with the schools and conferences they played in

--- createSchools.py
+++ createSchools.py
@@ -1,7 +1,21 @@
 import json
 
+school_json = 'schoolData.json'
+
 with open('conferenceData.json') as f:
   data = json.load(f)
 
+schoolArray = []
+
 for conference in data:
-  print(conference["conference"])
+    for school in conference["schools"]:
+        obj = {"school": school["school"], "conferences": [{"conference": conference["conference"], "years": school["years"]}]}
+        if obj["school"] not in [entry["school"] for entry in schoolArray]:
+            schoolArray.append(obj)
+        else:
+            for entry in schoolArray:
+                if entry["school"] == obj["school"]:
+                    entry["conferences"].append({"conference": conference["conference"], "years": school["years"]})
+
+with open(school_json, 'w') as handle:
+  json.dump(schoolArray, handle, separators=(',', ':'), indent=None)
